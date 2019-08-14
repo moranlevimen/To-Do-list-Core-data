21e45fbcae80e12128d093e5a0579644814862ae
moran <moran_brian@yahoo.com>
2019-08-14 08:17:23 +0300

    first commit

---
+++ ToDoApp/Model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  t.swift
+//  ToDoApp
+//
+//  Created by moran levi on 12/08/2019.
+//  Copyright © 2019 moran levi. All rights reserved.
+//
+
+import Foundation
+struct TodoItem {
+    var title: String = ""
+    var done: Bool = false
+}
